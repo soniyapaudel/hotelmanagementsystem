f2fa64041a147e79fc87b72021ec49959b0b5e21
soniyapaudel <sunnapaudel@gmail.com>
2022-06-20 21:58:56 +0545

    initial commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Crowne_Plaza.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ViewResult Signup()
+        {
+            return View();
+        }
+        public ViewResult login()
+        {
+            return View();
+        }
+        public ViewResult Dashboard()
+        {
+            return View();
+        }
+        public ViewResult AddRoom()
+        {
+            return View();
+        }
+        public ViewResult About()
+        {
+            return View();
+        }
+        public ViewResult Contact()
+        {
+            return View();
+        }
+        public ViewResult Admin()
+        {
+            return View();
+        }
+        public ViewResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Crowne_Plaza.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Crowne_Plaza.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
